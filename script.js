af2266765ceb06bdd17a5196b0216c267fa67b19
Tringlyman <makiskollias12@gmail.com>
2025-06-19 13:11:33 +0000

    innitial commit

--- script.js
+++
@@ -1,9 +0,0 @@
-const avaibleDocs = ["Tringlyman_s_JSON_Utilities"]
-function DocURIs(ext){
-    if(avaibleDocs.includes(ext)){
-        window.open('/documents/' + ext + ".html" ,"_blank")
-    }
-    else{
-        alert('invalid!')
-    }
-}
