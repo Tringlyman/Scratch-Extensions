6a08e89c82ebb94ca1418a3223665eefba7a58fe
Tringlyman <makiskollias12@gmail.com>
2025-06-19 14:32:01 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function CopyClipboard(text) {
-    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
+    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/codespace-orange-orbit-7vrgj5vr5p7jhxj9/Extensions/${text}`)
 }
