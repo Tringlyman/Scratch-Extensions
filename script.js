35772893b002bb0f8efa72d1533588da213b90ab
Tringlyman <tringlyman@gmail.com>
2025-06-20 19:03:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function CopyClipboard(text) {
-    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/codespace-orange-orbit-7vrgj5vr5p7jhxj9/Extensions/${text}`)
+    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
 }
 
 function Docs(text) {
-     if (`${text}` != null || `${text}` != undefined || ``${text} != ``){
-         const Window = window.open(`https://github.com/Tringlyman/Scratch-Extensions/blob/main/Docs/${text}.md`, "_blank")
-     }
-     else{
-        alert("This Extension doesn't have a Document yet!")
-    }
+     const Window = window.open(`https://github.com/Tringlyman/Scratch-Extensions/blob/main/Docs/${text}.md/`)
 }
