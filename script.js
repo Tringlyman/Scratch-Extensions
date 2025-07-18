80dc1b107fcc912c19b84d348fd34e2300d83cb9
Tringlyman <tringlyman@gmail.com>
2025-07-18 22:12:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function CopyClipboard(text) {
-    navigator.clipboard.writeText(window.location.origin + `/Extensions/${text}`)
+    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
 }
 
 function Docs(text) {
