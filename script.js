c8cdaf39c4f36a2b46fc741ffdb41649228e37a6
Tringlyman <tringlyman@gmail.com>
2025-07-18 22:28:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function CopyClipboard(text) {
+    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
+}
+
+function Docs(text) {
+    window.open(`https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/${text}.md/`)
+}
