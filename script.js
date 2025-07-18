8a8856517f97204346ba73940cc071a100f05901
Tringlyman <tringlyman@gmail.com>
2025-07-18 22:28:07 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function CopyClipboard(text) {
-    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
-}
-
-function Docs(text) {
-    window.open(`https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/${text}.md/`)
-}
