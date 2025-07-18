83332b8be869fa745a919242377e78afe6e86b55
Tringlyman <tringlyman@gmail.com>
2025-07-18 22:25:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function CopyClipboard(text) {
-    const rawuser = `https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`
-    navigator.clipboard.writeText(rawuser)
+    navigator.clipboard.writeText(`https://raw.githubusercontent.com/Tringlyman/Scratch-Extensions/refs/heads/main/Extensions/${text}`)
 }
 
 function Docs(text) {
-    const doc = `https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/${text}.md/`
-    window.open(doc)
+    window.open(`https://github.com/Tringlyman/docs.Scratch-Extensions/blob/main/Docs/${text}.md/`)
 }
